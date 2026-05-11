6c5b0cfd4bbebffde010e560811d59de6f226414
Rus <frozenrus32@gmail.com>
2026-05-11 18:20:40 +0800

    Change tab space type

--- scenes/level/level.gd
+++ scenes/level/level.gd
@@ -11,17 +11,17 @@ var _using_fantasy_domain: bool = false
 
 ## Sets the active domain to the fantasy domain.
 func use_fantasy_domain(enable: bool) -> void:
-    if enable:
-        fantasy_domain.set_enabled(true)
-        real_domain.set_enabled(false)
-        active_domain = fantasy_domain
-        pass
+	if enable:
+		fantasy_domain.set_enabled(true)
+		real_domain.set_enabled(false)
+		active_domain = fantasy_domain
+		pass
 
-    else:
-        fantasy_domain.set_enabled(false)
-        real_domain.set_enabled(true)
-        active_domain = real_domain
-        pass
+	else:
+		fantasy_domain.set_enabled(false)
+		real_domain.set_enabled(true)
+		active_domain = real_domain
+		pass
 
-    _using_fantasy_domain = enable
-    return
+	_using_fantasy_domain = enable
+	return
