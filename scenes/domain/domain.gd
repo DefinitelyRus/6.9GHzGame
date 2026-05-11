9b8f8fa3b6daf67d8b3b986e661350a1f647e637
Rus <frozenrus32@gmail.com>
2026-05-11 18:18:03 +0800

    Add set_enabled method

--- scenes/domain/domain.gd
+++ scenes/domain/domain.gd
@@ -5,3 +5,14 @@ extends Node2D
 @export_group("Components")
 @export var tilemap: TileMap
 @export var world_objects: Node2D
+var level: Level = null # Set by the Level itself.
+
+@export_group("Visibility")
+## The default layer is the integer representation of the `visibility_layer` property.
+@export var default_layer: int = 0
+
+
+func set_enabled(enable: bool) -> void:
+	Log.me("Setting enabled = %s for domain %s from level %s." % [str(enable), name, level.name])
+	visibility_layer = default_layer if enable else 0
+	return
