f826e8a14757c0c27e86e1ee5ba6313a2bb63b6e
Rus <frozenrus32@gmail.com>
2026-05-11 15:10:54 +0800

    Added basic members

--- scenes/master/master.gd
+++ scenes/master/master.gd
@@ -1,5 +1,22 @@
 class_name Master
 extends Node
 
-func _ready() -> void:
-	pass
+@export_group("Nodes & Components")
+@export var background: TextureRect
+
+@export_group("Debugging")
+@export var log_ready: bool = false
+
+static var instance: Master
+static var is_paused: bool = false
+
+
+# ---------- GODOT CALLBACKS ----------
+func _enter_tree() -> void:
+	if instance != null:
+		Log.err("Another Master instance already exists! This instance will remove itself.", log_ready)
+		queue_free()
+		return
+		
+	instance = self
+	return
