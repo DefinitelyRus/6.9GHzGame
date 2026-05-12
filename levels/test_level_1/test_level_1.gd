9a5450f6b5ce495f590ea975968ff156a7f6f8c5
Rus <frozenrus32@gmail.com>
2026-05-12 19:17:50 +0800

    Refactor/code cleanup

--- levels/test_level_1/test_level_1.gd
+++ levels/test_level_1/test_level_1.gd
@@ -3,9 +3,8 @@ extends Level
 
 func _ready() -> void:
 	super._ready()
-	#camera.set_target_topleft(Vector2.ZERO, true)
 	
-	var player: Player = real_domain.world_objects.get_child(0) as Player
-	Log.me("Setting player %s as the target..." % player.name)
+	var player: Player = irl_domain.world_objects.get_child(0) as Player
+	Log.me("Setting %s as the target..." % player.name)
 	camera.set_target_centered(player.global_position, true)
 	pass
