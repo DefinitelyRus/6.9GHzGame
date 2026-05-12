69d3c95339863798b6c4b748d83c464484dd54f8
Rus <frozenrus32@gmail.com>
2026-05-12 22:03:13 +0800

    Support IRL/VR sprites and domain switching

--- scenes/character/character_animation.gd
+++ scenes/character/character_animation.gd
@@ -1,34 +1,27 @@
 class_name CharacterAnimation
 extends Node
 
-@export var character: Character
 @export var irl_animated_sprite: AnimatedSprite2D
 @export var vr_animated_sprite: AnimatedSprite2D
+var character: Character = null
 
 
 # ---------- ANIMATION HANDLING ----------
-func _resolve_dependencies() -> void:
-	if character == null: character = get_parent() as Character
-		
-	if character != null and animated_sprite == null:
-		var n: Node = character.get_node_or_null("AnimatedSprite2D")
-		animated_sprite = n as AnimatedSprite2D
-		pass
-
-	return
-
-
 func _update_facing_direction() -> void:
-	if character == null or animated_sprite == null: return
+	if character == null or irl_animated_sprite == null or vr_animated_sprite == null: return
 		
 	var facing_direction: int = character.facing_direction
-	if facing_direction > 0: animated_sprite.flip_h = false
-	elif facing_direction < 0: animated_sprite.flip_h = true
+
+	if facing_direction > 0: irl_animated_sprite.flip_h = false
+	elif facing_direction < 0: irl_animated_sprite.flip_h = true
+
+	if facing_direction > 0: vr_animated_sprite.flip_h = false
+	elif facing_direction < 0: vr_animated_sprite.flip_h = true
 	return
 
 
 func _update_animation_state() -> void:
-	if character == null or animated_sprite == null: return
+	if character == null or irl_animated_sprite == null or vr_animated_sprite == null: return
 		
 	var combat_handler: Node = character.combat_handler
 	if combat_handler != null:
@@ -37,9 +30,28 @@ func _update_animation_state() -> void:
 			pass
 		pass
 
-	if not character.is_on_floor(): animated_sprite.play("Jump")
-	elif abs(character.velocity.x) > 10.0: animated_sprite.play("Run")
-	else: animated_sprite.play("Idle")
+	if not character.is_on_floor(): irl_animated_sprite.play("Jump")
+	elif abs(character.velocity.x) > 10.0: irl_animated_sprite.play("Run")
+	else: irl_animated_sprite.play("Idle")
+
+	if not character.is_on_floor(): vr_animated_sprite.play("Jump")
+	elif abs(character.velocity.x) > 10.0: vr_animated_sprite.play("Run")
+	else: vr_animated_sprite.play("Idle")
+	return
+
+
+func _on_level_domain_switched(switched_to_vr: bool) -> void:
+	if switched_to_vr:
+		vr_animated_sprite.show()
+		irl_animated_sprite.hide()
+
+		pass
+
+	else:
+		vr_animated_sprite.hide()
+		irl_animated_sprite.show()
+		pass
+		
 	return
 
 
@@ -51,11 +63,16 @@ func _update_animation_state() -> void:
 # ---------- GODOT CALLBACKS ----------
 func _ready() -> void:
 	Log.me("Readying character animation %s. Scanning properties..." % name, log_ready)
-	_resolve_dependencies()
+
+	character = get_parent() as Character
 
 	if character == null:
 		Log.err("character is missing; animation won't work.")
 		return
+	
+	else:
+		var level: Level = character.current_level
+		level.domain_switched.connect(_on_level_domain_switched)
 
 	if irl_animated_sprite == null:
 		Log.warn("irl_animated_sprite missing; IRL animations won't work.")
