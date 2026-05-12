5b5de8e5d5938bb06d4d8dec812191add4c4331e
Rus <frozenrus32@gmail.com>
2026-05-12 08:34:10 +0800

    Add camera_manager members

--- scenes/master/camera_manager.gd
+++ scenes/master/camera_manager.gd
@@ -1,1 +1,14 @@
+class_name CameraManager
 extends Camera2D
+
+static var instance: CameraManager = null
+
+func _enter_tree() -> void:
+	Log.me("A CameraManager has entered the tree. Scanning...")
+	if instance != null:
+		Log.err("Existing instance of CameraManager detected.")
+		queue_free()
+		return
+		
+	instance = self
+	return
