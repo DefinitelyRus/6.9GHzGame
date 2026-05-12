587c670952270621c6a4f2e5d5a7e4790918ae2c
Rus <frozenrus32@gmail.com>
2026-05-12 18:40:21 +0800

    Add test camera movement

--- levels/test_level_1/test_level_1.gd
+++ levels/test_level_1/test_level_1.gd
@@ -3,6 +3,9 @@ extends Level
 
 func _ready() -> void:
 	super._ready()
+	#camera.set_target_topleft(Vector2.ZERO, true)
 	
-	camera.target
+	var player: Player = real_domain.world_objects.get_child(0) as Player
+	Log.me("Setting player %s as the target..." % player.name)
+	camera.set_target_centered(player.global_position, true)
 	pass
