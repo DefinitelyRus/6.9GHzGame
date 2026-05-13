da6c51d83c590b99be34fd50d4a6ace63de8dc64
Rus <frozenrus32@gmail.com>
2026-05-13 08:06:47 +0800

    Remove comment

--- scenes/projectile/slingshot_projetile/slingshot_projectile.gd
+++ scenes/projectile/slingshot_projetile/slingshot_projectile.gd
@@ -1,6 +1,2 @@
 class_name SlingshotProjectile
 extends Projectile
-
-# SlingshotProjectile inherits all functionality from Projectile
-# Add slingshot-specific behavior here if needed in the future
-
